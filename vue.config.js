9d84604fab760fb8d4ca8dfbf4be48cb62de2e79
Nicolas Corral <nicolas.corral@ubisoft.com>
2021-06-23 00:19:52 -0400

    fix: publicPath

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,7 @@
+// eslint-disable-next-line
+const packageJSON = require("./package.json");
+
 module.exports = {
-  publicPath: process.env.NODE_ENV === "production" ? "./" : "/",
+  publicPath:
+    process.env.NODE_ENV === "production" ? `/${packageJSON.name}/` : "/",
 };
