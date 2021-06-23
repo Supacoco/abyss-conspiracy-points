9768a5a32007ea1f1cd186cb06e374a08e09003f
Nicolas Corral <nicolas.corral@ubisoft.com>
2021-06-23 00:35:25 -0400

    test

---
+++ precache-manifest.5ca5fcf39cf683faa8ab19eb000de5fe.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae1702f9ff4206817086",
+    "url": "/abyss-conspiracy-points/css/app.601d2ada.css"
+  },
+  {
+    "revision": "812fd7859b310340b3d6",
+    "url": "/abyss-conspiracy-points/css/chunk-0196de6a.828f472f.css"
+  },
+  {
+    "revision": "da96ba92303c1ba860d4",
+    "url": "/abyss-conspiracy-points/css/chunk-04dc87af.b2247972.css"
+  },
+  {
+    "revision": "eef004e9f6c84efe5001065eef195e98",
+    "url": "/abyss-conspiracy-points/debug.log"
+  },
+  {
+    "revision": "b80319798b00dd3a96b1924e37c3af06",
+    "url": "/abyss-conspiracy-points/index.html"
+  },
+  {
+    "revision": "ae1702f9ff4206817086",
+    "url": "/abyss-conspiracy-points/js/app.1a9db419.js"
+  },
+  {
+    "revision": "812fd7859b310340b3d6",
+    "url": "/abyss-conspiracy-points/js/chunk-0196de6a.f654837a.js"
+  },
+  {
+    "revision": "da96ba92303c1ba860d4",
+    "url": "/abyss-conspiracy-points/js/chunk-04dc87af.8019f02f.js"
+  },
+  {
+    "revision": "aad37e8d348c4ae5ef2c",
+    "url": "/abyss-conspiracy-points/js/chunk-487f3e3c.01cc536e.js"
+  },
+  {
+    "revision": "0d6ca7ac3e3e5d90ba19",
+    "url": "/abyss-conspiracy-points/js/chunk-vendors.722be4d6.js"
+  },
+  {
+    "revision": "be933cd356731464000061ea8f7bf110",
+    "url": "/abyss-conspiracy-points/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/abyss-conspiracy-points/robots.txt"
+  }
+]);
